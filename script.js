3c603c2b60ff98456cd2d2b146f4fbec580396b3
Kaylan Wang <kaylanwang5@gmail.com>
2023-06-16 15:45:08 -0400

    gameLoop complete, now plays rps to 5 wins

--- script.js
+++ script.js
@@ -6,32 +6,67 @@ function getComputerChoice() {
         case 1:
             return "PAPER"
         case 2:
-            return "SCISSORS"
+            return "SCISSOR"
     }
 }
 
 function playRound(player, comp) {
-    const win = `You Win! ${player.toUpperCase()} beats ${comp}`
-    const lose = `You Lost :( ${comp} beats ${player.toUpperCase()}`
-    if (player.toUpperCase() === "ROCK" && comp === "SCISSORS") {
-        return win
-    } else if (player.toUpperCase() === "ROCK" && comp === "PAPER") {
-        return lose
-    } else if (player.toUpperCase() === "PAPER" && comp === "ROCK") {
-        return win
-    } else if (player.toUpperCase() === "PAPER" && comp === "SCISSORS") {
-        return lose
-    } else if (player.toUpperCase() === "SCISSORS" && comp === "PAPER") {
-        return win
-    } else if (player.toUpperCase() === "SCISSORS" && comp === "ROCK") {
-        return lose
+    if (player === "ROCK" && comp === "SCISSOR") {
+        return 0
+    } else if (player === "ROCK" && comp === "PAPER") {
+        return 1
+    } else if (player === "PAPER" && comp === "ROCK") {
+        return 0
+    } else if (player === "PAPER" && comp === "SCISSOR") {
+        return 1
+    } else if (player === "SCISSOR" && comp === "PAPER") {
+        return 0
+    } else if (player === "SCISSOR" && comp === "ROCK") {
+        return 1
     } else {
-        return "TIE"
+        return 2
     }
 }
 
-const playerChoice = "rock"
-const compSelection = getComputerChoice()
-console.log(`player choice: ${playerChoice.toUpperCase()}`)
-console.log(`computer choice: ${compSelection}`)
-console.log(playRound(playerChoice, compSelection))
+function gameLoop(){
+    
+    let playerCount = 0
+    let compCount = 0
+    let compSelection
+
+    while (playerCount < 5 && compCount < 5){
+        let playerSelection = prompt("Enter rock, paper, or scissor:")
+        const playerSelection1 = playerSelection.toUpperCase().trim()
+        compSelection = getComputerChoice()
+        const win = `You Win! ${playerSelection1} beats ${compSelection}`
+        const lose = `You Lost :( ${compSelection} beats ${playerSelection1}`
+
+        if (playerSelection1 === "ROCK" ||
+            playerSelection1 === "PAPER" ||
+            playerSelection1 === "SCISSOR"
+        ) {
+            let gameResult = playRound(playerSelection1, compSelection)
+            if (gameResult === 0){
+                console.log(win)
+                console.log(`Current score: Player: ${++playerCount} Computer: ${compCount}`)
+            } else if (gameResult === 1) {
+                console.log(lose)
+                console.log(`Current score: Player: ${playerCount} Computer: ${++compCount}`)
+            } else {
+                console.log("Tie")
+                console.log(`Current score: Player: ${playerCount} Computer: ${compCount}`)
+            }
+        } else {
+            console.error("Invalid Input. Try again")
+            continue
+        }
+
+    }
+    if (playerCount > compCount) {
+        console.log("You beat the computer! You must've gotten lucky...")
+    } else {
+        console.log("You lost lmaooo, ur trash kid ")
+    }
+}
+
+gameLoop()
