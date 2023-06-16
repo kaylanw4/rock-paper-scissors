1bf103fd314df687e4988afbf07597a25ffeda23
Kaylan Wang <kaylanwang5@gmail.com>
2023-06-16 13:38:17 -0400

    Small changes to help readablility

--- script.js
+++ script.js
@@ -11,24 +11,20 @@ function getComputerChoice() {
 }
 
 function playRound(player, comp) {
+    const win = `You Win! ${player.toUpperCase()} beats ${comp}`
+    const lose = `You Lost :( ${comp} beats ${player.toUpperCase()}`
     if (player.toUpperCase() === "ROCK" && comp === "SCISSORS") {
-        return `You Win! ${player.toUpperCase()} beats ${comp}`
-
+        return win
     } else if (player.toUpperCase() === "ROCK" && comp === "PAPER") {
-        return `You Lost :( ${comp} beats ${player.toUpperCase()}`
-
+        return lose
     } else if (player.toUpperCase() === "PAPER" && comp === "ROCK") {
-        return `You Win! ${player.toUpperCase()} beats ${comp}`
-
+        return win
     } else if (player.toUpperCase() === "PAPER" && comp === "SCISSORS") {
-        return `You Lost :( ${comp} beats ${player.toUpperCase()}`
-
+        return lose
     } else if (player.toUpperCase() === "SCISSORS" && comp === "PAPER") {
-        return `You Win! ${player.toUpperCase()} beats ${comp}`
-
+        return win
     } else if (player.toUpperCase() === "SCISSORS" && comp === "ROCK") {
-        return `You Lost :( ${comp} beats ${player.toUpperCase()}`
-
+        return lose
     } else {
         return "TIE"
     }
@@ -36,5 +32,6 @@ function playRound(player, comp) {
 
 const playerChoice = "rock"
 const compSelection = getComputerChoice()
+console.log(`player choice: ${playerChoice.toUpperCase()}`)
 console.log(`computer choice: ${compSelection}`)
 console.log(playRound(playerChoice, compSelection))
