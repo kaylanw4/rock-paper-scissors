ae1518ace649619ad9d689112c5b8641e4d1e9ab
Kaylan Wang <kaylanwang5@gmail.com>
2023-06-18 15:28:12 -0400

    added getScore to display overall tally to webpage

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 
 
+// Button on click event listeners
 const btns = document.querySelectorAll("button")
 btns.forEach(btn => btn.addEventListener('click', playRound))
 
+// Results div
 const results = document.querySelector("#results")
+
+// add results of individual game
 const p = document.createElement('p');
 results.appendChild(p)
+
+// add tallied score for player and computer
+const score = document.createElement('p')
+results.appendChild(score)
+
+// initialize global variables
+let playerScore = 0
+let compScore = 0
+
 
 /**
  * Returns a random choice between rock, paper, scissors
@@ -35,17 +48,39 @@ function playRound(e) {
 
     if (player === "rock" && comp === "scissor") {
         p.textContent = win
+        getScore(0)
     } else if (player === "rock" && comp === "paper") {
         p.textContent = lose
+        getScore(1)
     } else if (player === "paper" && comp === "rock") {
         p.textContent = win
+        getScore(0)
     } else if (player === "paper" && comp === "scissor") {
         p.textContent = lose
+        getScore(1)
     } else if (player === "scissor" && comp === "paper") {
         p.textContent = win
+        getScore(0)
     } else if (player === "scissor" && comp === "rock") {
         p.textContent = lose
+        getScore(1)
     } else {
         p.textContent = "Tie"
+        getScore(2)
+    }
+}
+
+function getScore(result) {
+    
+// let currScore = `Player: ${playerScore} Computer: ${compScore}`
+
+    if (result === 0){
+        playerScore++
+        score.textContent = `Player: ${playerScore} Computer: ${compScore}`
+    } else if (result === 1){
+        compScore++
+        score.textContent = `Player: ${playerScore} Computer: ${compScore}`
+    } else {
+        score.textContent = `Player: ${playerScore} Computer: ${compScore}`
     }
 }
